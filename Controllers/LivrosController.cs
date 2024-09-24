77c1b2ae19f0543159220eeb605156b6f3202a06
Pedro WSL <pdro5k@outlook.com>
2024-09-24 19:03:35 -0300

    Projeto iniciado

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,56 @@
+﻿using GerencimentoBiblioteca.Entities;
+using GerencimentoBiblioteca.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerencimentoBiblioteca.Controllers;
+
+[ApiController]
+[Route("api/livros/")]
+
+public class LivrosController : ControllerBase
+{
+    private readonly LivrosDbContext _contexto;
+
+    // Injeção de depedências
+    public LivrosController(LivrosDbContext contexto)
+    {
+        _contexto = contexto;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var livros = _contexto.Livros;
+        return Ok(livros);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        throw new Exception();
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Post(Livro livro)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var livro = _contexto.Livros.SingleOrDefault(e => e.Id == id);
+
+        if (livro == null)
+        {
+            return NotFound();
+        }
+        
+        _contexto.Livros.Remove(livro);
+        _contexto.SaveChanges();
+        
+        return NoContent();
+    }
+    
+}
---
+++ Persistence/LivrosDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GerencimentoBiblioteca.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerencimentoBiblioteca.Persistence;
+
+public class LivrosDbContext : DbContext
+{
+    public LivrosDbContext(DbContextOptions<LivrosDbContext> options) 
+        : base(options)
+    {
+        
+    }
+        
+    
+    public DbSet<Livro> Livros { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Livro>()
+            .HasKey(e => e.Id);
+    }
+    
+}
